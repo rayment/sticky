5b60d0cfc9d9ded32dff852458c7f9f9edafef8c
Finn Rayment <finn@rayment.fr>
2022-02-23 23:29:45 +0100

    Ifdef comments.

--- include/sticky/common/defines.h
+++ include/sticky/common/defines.h
@@ -44,9 +44,9 @@
 /* GNU / MSVC detection and relevant macros */
 #if defined(__GNUC__)
 #define PACK(...) __VA_ARGS__ __attribute__((__packed__))
-#elif defined(_MSC_VER)
+#elif defined(_MSC_VER) /* __GNUC__ and _MSC_VER */
 #define PACK(...) __pragma(pack(push, 1)) __VA_ARGS__ __pragma(pack(pop))
-#endif
+#endif /* __GNUC__ and _MSC_VER */
 
 #endif /* FR_RAYMENT_STICKY_DEFINES_H */
 
