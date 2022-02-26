f40ed6150d4e06b3ae27b1318e08334741b81944
Finn Rayment <finn@rayment.fr>
2022-02-26 23:47:51 +0100

    Added skeleton frustum struct.
    Added a skeleton struct that contains a frustum that will be used for
    frustum-culling.

---
+++ include/sticky/math/frustum.h
@@ -0,0 +1,38 @@
+/*
+ * frustum.h
+ * Culling frustum math header.
+ *
+ * Author       : Finn Rayment <finn@rayment.fr>
+ * Date created : 26/02/2022
+ */
+
+#ifndef FR_RAYMENT_STICKY_FRUSTUM_H
+#define FR_RAYMENT_STICKY_FRUSTUM_H 1
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include "sticky/common/types.h"
+#include "sticky/math/vec3.h"
+
+typedef struct
+Sfrustum_s
+{
+	Svec3 fnear[3], ffar[3], center;
+} Sfrustum;
+
+Sfrustum *S_frustum_new(void);
+void      S_frustum_delete(Sfrustum *);
+
+Sbool     S_frustum_intersects_point(const Sfrustum *, const Svec3 *);
+Sbool     S_frustum_intersects_bounds(const Sfrustum *,
+                                      const Svec3 *, const Svec3 *);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* FR_RAYMENT_STICKY_FRUSTUM_H */
+
