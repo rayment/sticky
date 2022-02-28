064b8d127dea5b4812338982859ae5251f2505e7
Finn Rayment <finn@rayment.fr>
2022-02-28 23:10:21 +0100

    Some work on frustum code.

--- src/math/frustum.c
+++ src/math/frustum.c
@@ -6,12 +6,38 @@
  * Date created : 27/02/2022
  */
 
+#include "sticky/common/error.h"
 #include "sticky/common/types.h"
 #include "sticky/math/frustum.h"
 #include "sticky/math/vec3.h"
+#include "sticky/memory/allocator.h"
 
-Sfrustum *S_frustum_new(void);
-void      S_frustum_delete(Sfrustum *);
+Sfrustum *
+S_frustum_new(void)
+{
+	Sfrustum *frustum;
+	Suint8 i;
+	frustum = S_memory_new(sizeof(Sfrustum));
+	for (i = 0; i < 3; ++i)
+	{
+		_S_CALL("S_vec3_zero", S_vec3_zero(frustum->fnear+i));
+		_S_CALL("S_vec3_zero", S_vec3_zero(frustum->ffar+i));
+	}
+	_S_CALL("S_vec3_zero", S_vec3_zero(&frustum->center));
+	memset(frustum->planes, 0, sizeof(frustum->planes));
+	return frustum;
+}
+
+void
+S_frustum_delete(Sfrustum *frustum)
+{
+	if (!frustum)
+	{
+		_S_SET_ERROR(S_INVALID_VALUE, "S_frustum_delete");
+		return;
+	}
+	S_memory_delete(frustum);
+}
 
 Sbool     S_frustum_intersects_point(const Sfrustum *, const Svec3 *);
 Sbool     S_frustum_intersects_bounds(const Sfrustum *,
